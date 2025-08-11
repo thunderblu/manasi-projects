22ff50b3035261d137f3796075e07c863968eefa
thunderblu <bondalapati.m@northeastern.edu>
2025-08-11 10:01:23 -0400

    Remove placeholder for MLOps folder

--- Mlops-stock prediction/terraform/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "322603165747-terraform-state"
-    prefix = "vertex-ai/state"
-  }
-} 
--- Mlops-stock prediction/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "GCP Project ID"
-  default     = "team02-441605"
-}
-
-variable "region" {
-  description = "GCP Region"
-  default     = "us-east1"
-} 
